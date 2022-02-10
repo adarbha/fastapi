fecb464769d0cc15f00f4dc05b8ad3620c40cf41
Arjun Darbha <arjun@eatsleepride.com>
2022-02-09 23:11:12 -0500

    Function handling database connection to AzureSQL db. This is a test

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
+import os
+import json
+import pymssql
+import random
+from dotenv import load_dotenv
 from fastapi import FastAPI
 
 app = FastAPI()
 
-#domain where this api is hosted for example : localhost:5000/docs to see swagger documentation automagically generated.
+load_dotenv()
+
+
+# domain where this api is hosted for example : localhost:5000/docs to see swagger documentation automagically generated.
 
 
 @app.get("/")
 def home():
-    return {"message":"Hello TutLinks.com"}
+    return {"message": "Hello TutLinks.com"}
+
+
+@app.get("/run_query/")
+def run_sql_query():
+    with pymssql.connect(os.getenv("SERVER"), os.getenv("DVSUSERNAME"), os.getenv("PASSWORD"),
+                         os.getenv("DEV_DB")) as conn:
+        with conn.cursor(as_dict=True) as cursor:
+            cursor.execute("SELECT TOP 5 CustomerID, CompanyName from SalesLT.Customer")
+            rows = cursor.fetchall()
+            return json.dumps(rows[random.choice(range(5))])
+
+
+if __name__ == "__main__":
+    print(run_sql_query())
